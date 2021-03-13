3cf027856317f9b66b8c7bc2dab7d84dd197eff2
Xplous <nick.filippoff2016@yandex.ru>
2021-03-13 21:12:27 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig) { 
+    let arr = []; 
+    for (let i of bracketsConfig) { 
+    i = i.toString().replace(",", ""); 
+    arr.push(i); 
+    } 
+    let test = (string) => { 
+    for (let j of arr) { 
+    if (string.includes(j)) { 
+    string = string.replace(j, ""); 
+    } 
+    } 
+    str = string; 
+    for (let i of arr) { 
+    while (str.includes(i)) { 
+    test(str); 
+    } 
+    } 
+    } 
+    test(str); 
 }

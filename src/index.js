7535e7f397b170a275c06fca65e80eda85157d76
Nikita <68853033+Xplous@users.noreply.github.com>
2023-01-25 23:31:06 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 module.exports = function check(str, bracketsConfig) { 
     let arr = []; 
     for (let i of bracketsConfig) { 
-    i = i.toString().replace(",", ""); 
-    arr.push(i); 
+        i = i.toString().replace(",", ""); 
+        arr.push(i); 
     } 
     let test = (string) => { 
-    for (let j of arr) { 
-    if (string.includes(j)) { 
-    string = string.replace(j, ""); 
-    } 
-    } 
-    str = string; 
-    for (let i of arr) { 
-    while (str.includes(i)) { 
-    test(str); 
-    } 
-    } 
+        for (let j of arr) { 
+            if (string.includes(j)) { 
+                string = string.replace(j, ""); 
+            } 
+        } 
+        str = string; 
+        for (let i of arr) { 
+            while (str.includes(i)) { 
+                test(str); 
+            } 
+        } 
     } 
     test(str); 
     return str.length === 0
